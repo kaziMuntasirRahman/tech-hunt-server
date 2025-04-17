1727a311c70ece7e3feb682948dfdc69044a4c7e
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2025-04-17 15:27:06 +0600

    google sign in user info is being posted to db

--- api/user/add_new_property.js
+++ api/user/add_new_property.js
@@ -6,7 +6,7 @@ router.patch('/users/add-property', async (req, res) => {
   console.log('patch /users/add_property is being hit...')
   try {
     const { paymentCollection } = await connectDB()
-    const result = await paymentCollection.updateMany({}, {$set: {status:'pending'}}, {upsert: true})
+    const result = await paymentCollection.updateMany({}, { $unset: { ['provider']: "" } }, {upsert: true})
     return res.status(200).send(result)
   } catch (err) {
     return res.status(500).send({ message: err.message })
--- api/user/post_new_user.js
+++ api/user/post_new_user.js
@@ -1,26 +1,38 @@
-const express = require('express');
+const express = require('express')
 const router = express.Router()
 const connectDB = require('../../db/mongo_client')
 
 router.post('/users', async (req, res) => {
-  console.log("Post /users api is being hit...")
+  console.log('Post /users api is being hit...')
   try {
-    const newUser = req.body;
+    const newUser = req.body
     if (!newUser.email) {
-      return res.status(400).send({ message: "Bad Request" })
+      return res.status(400).send({ message: 'Bad Request' })
     }
+
+    const { userCollection } = await connectDB()
+    if (!newUser?.provider === 'google') {
+      const isExistingUser = await userCollection.findOne({
+        email: newUser.email
+      })
+      if (isExistingUser.email) {
+        return res.status(200).send(isExistingUser)
+      }
+    }
+
     newUser.createdAt = new Date()
     newUser.email = newUser.email.toLowerCase()
     newUser.isSubscribed = false
     newUser.hasPaidSubscription = false
     newUser.status = 'general'
-    const { userCollection } = await connectDB()
     const result = await userCollection.insertOne(newUser)
     return res.status(200).send(result)
   } catch (err) {
-    return res.status(500).send({ message: "Server Error!", error: err.message })
+    return res
+      .status(500)
+      .send({ message: 'Server Error!', error: err.message })
   } finally {
-    console.log("Post /users request finished.")
+    console.log('Post /users request finished.')
   }
 })
 
